443c026e16bebc979f01e31782cab8c0c8fe72ce
Heena Thadani <heenathadani14@gmail.com>
2024-03-02 10:52:03 -0500

    added a flexible interaction system

---
+++ Assets/Heena/Scripts/Interaction System/Interactor.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInteractable
+{
+    void Interact();
+}
+public class Interactor : MonoBehaviour
+{
+    public Transform interactorSource;
+    public float interactRange;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Ray r = new Ray(interactorSource.position, interactorSource.forward);
+            if(Physics.Raycast(r, out RaycastHit hitInfo, interactRange))
+            {
+                if(hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactObj))
+                {
+                    interactObj.Interact();
+                }
+            }
+        }
+    }
+}
