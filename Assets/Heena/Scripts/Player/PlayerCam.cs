eb1c3a82e918603c103ecb45712a22f2ba925ef0
James Zilberman <jaz1324576@gmail.com>
2024-03-04 20:59:21 -0500

    Enemy raycast height and player camera changes

--- Assets/Heena/Scripts/Player/PlayerCam.cs
+++ Assets/Heena/Scripts/Player/PlayerCam.cs
@@ -8,26 +8,25 @@ public class PlayerCam : MonoBehaviour
     public float sensY;
 
     public Transform orientation;
-    float xRotation;
+    float xRotation = 0f;
     float yRotation;
 
     private void Start()
     {
-        //Cursor.lockState = CursorLockMode.Locked;
-        //Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     private void Update()
     {
-        float mouseX = Input.GetAxisRaw("Mouse X") * sensX * Time.deltaTime;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * sensY * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * sensX * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensY * Time.deltaTime;
 
-        yRotation += mouseX;
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
-        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        orientation.Rotate(Vector3.up * mouseX);
 
     }
 }
